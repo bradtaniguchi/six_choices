f397260aae6e2fa6140ba1df7a16d655777d065b
bradtaniguchi <bradtaniguchi@gmail.com>
2018-06-13 19:48:27 -0700

    Added dotenv support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import * as fs from 'fs';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+if (fs.existsSync('.env')) {
+  require('dotenv').config();
+}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useStaticAssets(__dirname + '/../dist');
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
